d76014d67e059befe5530260df3a87b9077373bb
Christopher <christopher.vu.truong@gmail.com>
2019-12-06 16:41:36 -0500

    more img2world test img

--- image2world_test.py
+++ image2world_test.py
@@ -12,7 +12,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 
 canny = cv.Canny(gray, 50, 150, None, 3)
-contours,hier = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+contours,hier = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
 if len(contours) == 0:
     print("Didn't find any contours!")
@@ -21,13 +21,11 @@ if len(contours) == 0:
 for i, cnt in enumerate(contours):
     if cv.contourArea(cnt)>500:  # only grab large contours
         print('contour:', i)
+        #cv.drawContours(img,[cnt],0,(0,255,0),2)
         hull = cv.convexHull(cnt) # find the convex hull of contour
         hull = cv.approxPolyDP(hull,0.1*cv.arcLength(hull,True),True)
-        if len(hull)==4:
-            cv.drawContours(img,[hull],0,(0,255,0),2)
-            x,y,w,h = cv.boundingRect(cnt) # get minimal bounding for contour
-            #cv.drawContours(img, [cnt], -1, 0, -1)
-            break
-        #print(len(hull))
+        cv.drawContours(img,[hull],0,(0,255,0),2)
+
+    
 
 p.display(img)
